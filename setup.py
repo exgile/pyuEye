0737e2980180752815da43e95f4175a7ceede86b
jswon <mn2888@naver.com>
2017-06-23 13:23:44 +0900

    Basic control API for uEye XS camera.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyuEye',
+    version='1.0.0',
+    packages=[''],
+    url='github.com/jswon',
+    license='jswon',
+    author='jswon',
+    author_email='mn2888@naver.com',
+    description='Control ueye xs2 camera'
+)
